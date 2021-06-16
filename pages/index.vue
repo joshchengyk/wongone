4194368160ba11d388ba89491707b68fe95ecdc9
joshchengyk <joshchengyk@gmail.com>
2021-06-16 11:01:46 +0800

    interview

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,109 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        wongone
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class='body'>
+
+    <img src="/pictures/background image.png" alt="Image" class="bg">
+    <img src="/pictures/Stall image green.png" alt="Image" class="room">
+    <img src="/pictures/上一頁按鈕.png" alt="Image" class="pre-btn">
+    <img src="/pictures/廠商 logo.png" alt="Image" class="logo">
+    <img src="/pictures/廠商首頁圖上傳圖.jpg" alt="Image" class="screen">
+    <img src="/pictures/products list green.png" alt="Image" class='products-list-green '>
+    <div class='firm-name'>
+      流亞科技股份有限公司
+    </div>
+    <div class='firm-name-en'>
+      Logic Art Automation Co., Ltd
     </div>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .bg{
+    position:relative;
+    height:1080px;
+    width:1920px;
+    
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .room{
+    position:absolute;
+    height:996.2px;
+    width:1428px;
+    top:68.2px;
+    left:244.6px;
+  }
 
-.links {
-  padding-top: 15px;
+  .pre-btn{
+    position:absolute;
+    height:66.7px;
+    width:66.7px;
+    top:67.9px;
+    left:277px;
+  }
+
+  .logo{
+    position:absolute;
+    height:81.9px;
+    width:124.9px;
+    top:125.7px;
+    left:644.4px;
+  }
+
+  .screen{
+    position:absolute;
+    height:399.6px;
+    width:713.7px;
+    top:460.9px;
+    left:569.6px;
+  }
+
+  .firm-name{
+    position:absolute;
+    top:135.1px;
+    left:802px;
+    width: 561.3px;
+    height: 51px;
+    margin: 9.3px 20.8px 15.5px 32.8px;
+    font-family: MicrosoftYaHeiBold-GBpc-EUC-H;
+    font-size: 41.7px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.33;
+    letter-spacing: 8.35px;
+    text-align: center;
+    color: #333333;
+  }
+
+  .firm-name-en{
+    position:absolute;
+    top:201.6px;
+    left:802px;
+    width: 563.3px;
+    height: 34px;
+    margin: 15.5px 18.8px 225.3px 32.8px;
+    font-family: MicrosoftYaHeiGBpc-EUC-H;
+    font-size: 30.1px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 0.81;
+    letter-spacing: normal;
+    text-align: center;
+    color: #333333;
+
+  }
+
+ .products-list-green {
+    position:absolute;
+    top:907px;
+    left:782.4px;
+    width: 357.1px;
+    height: 87.6px;
+    margin: 46.5px 211.7px 0 13.2px;
+    object-fit: contain;
 }
+  
+  
+
 </style>
